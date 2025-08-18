401d2687b257e58eaa61d70ba9375bd6f16b4d2d
Lars0914 <dev.remote0914@gmail.com>
2025-08-18 03:43:36 -0700

    change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
           Hey, I'm Oleksii 👋
         </h2>
         <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-          AI Portfolio
+          Senior Web & AI & Automation Developer
         </h1>
       </motion.div>
 
