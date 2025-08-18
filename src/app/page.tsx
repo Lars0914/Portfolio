27dbbc9d0b710c7fb7197ecff1ba36286d5e3f49
Lars0914 <dev.remote0914@gmail.com>
2025-08-18 03:42:41 -0700

    remove git button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,15 +92,7 @@ export default function Home() {
       </div>
 
       {/* GitHub button */}
-      <div className="absolute top-6 right-8 z-20">
-        <GithubButton
-          //targetStars={68}
-          animationDuration={1.5}
-          label="Star"
-          size={'sm'}
-          repoUrl="https://github.com/Lars0914/Portfolio"
-        />
-      </div>
+
 
       <div className="absolute top-6 left-6 z-20">
         <button
